252b9292e0dcdf279585fc1253dfac42336593b3
paolocianchetti <paolo.cianchetti@gmail.com>
2023-09-06 16:13:05 +0200

    esercizio D2 aggiornato modulo React

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Welcome extends Component {
   render() {
     return (
-      <div className='mt-4 p-5 mb-4 bg-primary text-white rouded'>
+      <div className='mt-4 mb-4 p-5 bg-primary text-white rouded'>
         <h1>EpiBooks!</h1>
         <p>Sfoglia online la nostra collezione di libri fantastici.</p>
       </div>
