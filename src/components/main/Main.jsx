4646fa238fbb31960a7baf3b03a924989d62da48
paolocianchetti <paolo.cianchetti@gmail.com>
2023-09-06 16:06:57 +0200

    esercizio D2 aggiornato modulo React

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,15 +2,6 @@ import React, {Component} from "react";
 import LatestRelease from "../latestRelease/LatestRelease";
 
 class Main extends Component {
-
-    /* constructor(props) {
-        super(props);
-
-        this.state = {
-            data: this.props.data
-        }
-    } */
-
     render() {
         return (
             <div>
